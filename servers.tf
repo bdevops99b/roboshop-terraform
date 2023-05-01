fea317a3711359cd2271732046311c072fa5f439
bdevops99 <bdevops99@gmail.com>
2023-05-01 23:01:29 +0530

    terraform all roboshop instances

--- servers.tf
+++ servers.tf
@@ -1,3 +1,13 @@
+data "aws_ami" "centos" {
+  owners      = ["973714476881"]
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+}
+output "ami" {
+  value = data.aws_ami.centos.image_id
+}
+
+
 resource "aws_instance" "frontend" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
