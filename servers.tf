44d96a61dd4389e8a4401c6e572c25624e7fe278
bdevops99 <bdevops99@gmail.com>
2023-05-04 15:09:22 +0530

    terraform all roboshop instances

--- servers.tf
+++ servers.tf
@@ -12,16 +12,22 @@ variable "instance_type" {
   default = "t3.small"
 }
 
-resource "aws_instance" "frontend" {
+variable "components" {
+  default = [ "frontend","mongodb","catalogue" ]
+}
+
+resource "aws_instance" "instance" {
+  count = length(var.components)
   ami           = data.aws_ami.centos.image_id
   instance_type = var.instance_type
   vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
 
   tags = {
-    Name = "frontend"
+    Name = var.components[count.index]
   }
 }
 
+/*
 resource "aws_route53_record" "frontend" {
   zone_id = "Z09749362E9LBLZIEGY8G"
   name    = "frontend-dev.pand4u.online"
@@ -176,4 +182,4 @@ resource "aws_route53_record" "payment" {
   type    = "A"
   ttl     = 30
   records = [aws_instance.payment.private_ip]
-}
+}*/
