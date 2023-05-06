45d8e1e5624dbbaaf53d8314264d9df0beea9eba
bdevops99 <bdevops99@gmail.com>
2023-05-06 15:11:40 +0530

    terraform all roboshop instances

--- servers.tf
+++ servers.tf
@@ -1,33 +1,14 @@
 
 resource "aws_instance" "instance" {
-  for_each               = var.components
-  ami                    = data.aws_ami.centos.image_id
-  instance_type          = each.value["instance_type"]
-  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
+  for_each = var.components
+  ami      = data.aws_ami.centos.image_id
+  instance_type = each.value["instance_type"]
+  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
 
   tags = {
     Name = each.value["name"]
   }
-
-  provisioner "remote-exec" {
-
-    connection {
-      type     = "ssh"
-      user     = "centos"
-      password = "Devops321"
-      host     = self.private_ip
-    }
-    inline = [
-      "rm -rf roboshop-shell",
-      "git clone https://github.com/bdevops99b/roboshop-shell.git",
-      "cd roboshop-shell",
-      "sudo bash "${each.value["name"]}.sh"
-    ]
-
-  }
 }
-
-
 
 
 resource "aws_route53_record" "records" {
@@ -38,3 +19,4 @@ resource "aws_route53_record" "records" {
   ttl     = 30
   records = [aws_instance.instance[each.value["name"]].private_ip]
 }
+
