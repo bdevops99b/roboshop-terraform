2411dd5a5d980f4bfd34f618a9e3447e681a2a39
bdevops99 <bdevops99@gmail.com>
2023-05-07 09:08:57 +0530

    terraform all roboshop instances

--- servers.tf
+++ servers.tf
@@ -32,7 +32,7 @@ resource "aws_route53_record" "records" {
   for_each = var.components
   zone_id = "Z09749362E9LBLZIEGY8G"
   #name    = "${each.value["name"]}-dev.pand4u.online"
-  name    = "${each.value["name"]}"
+  name    = "${each.value["name"]}-dev"
   type    = "A"
   ttl     = 30
   records = [aws_instance.instance[each.value["name"]].private_ip]
