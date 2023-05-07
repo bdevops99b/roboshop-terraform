9f4cee8cf94b3737d020175eec775298101af6d2
bdevops99 <bdevops99@gmail.com>
2023-05-07 16:11:41 +0530

    terraform all roboshop instances

--- servers.tf
+++ servers.tf
@@ -4,6 +4,6 @@ module "servers" {
   component_name = each.value["name"]
   env            = var.env
   instance_type  = each.value["instance_type"]
-  password       = lookup(each.value,"password","null")
+  password       = lookup(each.value, "password", "null")
 }
 
