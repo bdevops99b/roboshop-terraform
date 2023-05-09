56133be3f5447023395d944ce05803bfb25b8ffa
bdevops99 <bdevops99@gmail.com>
2023-05-09 10:49:01 +0530

    ansible learn

--- servers.tf
+++ servers.tf
@@ -5,7 +5,7 @@ module "database-servers" {
   env            =  var.env
   instance_type  =  each.value["instance_type"]
   password       =  lookup(each.value, "password", "null")
-  provisioners   = true
+  provisioner    = true
 }
 
 module "app-servers" {
