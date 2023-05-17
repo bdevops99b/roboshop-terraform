9da96868447a2e54d6d2323f40b0f52517b1b7b4
bdevops99 <bdevops99@gmail.com>
2023-05-17 19:21:34 +0530

    ansible learn

--- module/locals.tf
+++ module/locals.tf
@@ -7,8 +7,9 @@ locals {
   "sudo bash ${var.component_name}.sh ${var.password}"
   ]
   app_commands = [
-    "sudo labauto ansible",
-    "ansible-pull -i localhost, -U https://github.com/bdevops99b/roboshop-ansible.git roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
+#    "sudo labauto ansible",
+#    "ansible-pull -i localhost, -U https://github.com/bdevops99b/roboshop-ansible.git roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
+"echo ok"
   ]
 
 }
