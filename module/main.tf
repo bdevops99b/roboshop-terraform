6cb9b452e29e991a96ddc3bcf550cc29cb3ab2c0
bdevops99 <bdevops99@gmail.com>
2023-05-17 19:12:59 +0530

    ansible learn

--- module/main.tf
+++ module/main.tf
@@ -52,3 +52,31 @@ resource "aws_iam_role" "role" {
     tag-key = "${var.component_name}-${var.env }-role"
   }
 }
+
+resource "aws_iam_role_policy" "ssm-ps-policy" {
+  name = "${var.component_name}-${var.env }-ssm-ps-policy"
+  role = aws_iam_role.role.id
+
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "VisualEditor0",
+        "Effect": "Allow",
+        "Action": [
+          "ssm:GetParameterHistory",
+          "ssm:GetParametersByPath",
+          "ssm:GetParameters",
+          "ssm:GetParameter"
+        ],
+        "Resource": "arn:aws:ssm:us-east-1:278280077392:parameter/${var.env}.${var.component_name}.*"
+      },
+      {
+        "Sid": "VisualEditor1",
+        "Effect": "Allow",
+        "Action": "ssm:DescribeParameters",
+        "Resource": "*"
+      }
+    ]
+  })
+}
