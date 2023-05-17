bfc0fd8d724cc533df0ea883fd9ba99135df9ae7
bdevops99 <bdevops99@gmail.com>
2023-05-17 18:54:26 +0530

    ansible learn

--- module/main.tf
+++ module/main.tf
@@ -30,3 +30,25 @@ resource "aws_route53_record" "records" {
   ttl     = 30
   records = [aws_instance.instance.private_ip]
 }
+
+resource "aws_iam_role" "role" {
+  name = "${var.component_name}-${var.env }-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "${var.component_name}-${var.env }-role"
+  }
+}
