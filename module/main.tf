c00ff950219689fd2dc13736d91e483b07874a46
bdevops99 <bdevops99@gmail.com>
2023-05-20 14:10:58 +0530

    ansible learn

--- module/main.tf
+++ module/main.tf
@@ -69,7 +69,7 @@ resource "aws_iam_role_policy" "ssm-ps-policy" {
         "Effect": "Allow",
         "Action": [
           "kms:GetParametersForImport",
-          "kms:Encrypt",
+          "kms:Decrypt",
           "ssm:GetParameterHistory",
           "ssm:GetParametersByPath",
           "ssm:GetParameters",
