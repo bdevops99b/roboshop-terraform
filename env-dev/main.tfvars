e51c958cf8250cd0abb6ed89ae70891769a41561
bdevops99 <bdevops99@gmail.com>
2023-05-31 23:57:31 +0530

    Vpcs -subnets creations

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -2,6 +2,12 @@ env = "dev"
 vpc = {
   main = {
     cidr_block = "10.0.0.0/16"
+    subnets = {
+      public = {
+        name = "public"
+        cidr_block = ["10.0.0.0/24", "10.0.1.0/24"]
+      }
+    }
 
   }
 }
--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ module "vpc" {
   source = "git::https://github.com/bdevops99b/tf-module-vpc.git"
   for_each = var.vpc
   cidr_block  = each.value["cidr_block"]
+  subnets = each.value["subnets"]
   tags = local.tags
   env = var.env
 }
