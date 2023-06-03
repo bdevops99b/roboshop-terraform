928b6b2e23dfa3afe8b1d58203585d2682a1b21c
bdevops99 <bdevops99@gmail.com>
2023-06-04 02:08:38 +0530

    Vpcs -subnets creations

--- main.tf
+++ main.tf
@@ -23,6 +23,6 @@ module "app" {
 
   subnet_ids = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
   vpc_id = lookup(lookup(module.vpc, "main", null), "vpc_id ", null)
-  allow_app_cidr =lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_app_cidr"], null), "subnet_cidrs", null)
+  allow_app_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_app_cidr"], null), "subnet_cidrs", null)
 }
 
