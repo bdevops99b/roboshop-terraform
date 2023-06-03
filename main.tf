51b3d75e942fa453669ac5bee271ee9a42cec671
bdevops99 <bdevops99@gmail.com>
2023-06-03 22:34:56 +0530

    Vpcs -subnets creations

--- main.tf
+++ main.tf
@@ -12,5 +12,5 @@ module "web" {
 
   for_each = var.app
   instance_type = each.value["instance_type"]
-  subnet_id = element(lookup(lookup(lookup(module.vpc, "main" , null), each.value["subnet_name"], null), "subnet_ids", null), 0)
+  subnet_id = element(lookup(lookup(lookup(lookup(module.vpc, "main" , null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null), 0)
 }
