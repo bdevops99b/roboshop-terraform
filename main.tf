566e6e730a7b21957b247d341a0a237fdb258fa8
bdevops99 <bdevops99@gmail.com>
2023-06-04 23:07:37 +0530

    Vpcs -subnets creations

--- main.tf
+++ main.tf
@@ -71,7 +71,7 @@ module "elasticcache" {
   subnets        = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
   allow_db_cidr  = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
   engine_version = each.value["engine_version"]
-  replicas_per_node_group = each.value["replicas_per_node_group "]
+  replicas_per_node_group = each.value["replicas_per_node_group"]
   num_node_groups         = each.value["num_node_groups"]
   node_type               = each.value["node_type"]
 
