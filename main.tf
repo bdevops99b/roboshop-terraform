dd9a1121795cf1bf53b93985bd397450494908cc
bdevops99 <bdevops99@gmail.com>
2023-06-04 18:26:30 +0530

    Vpcs -subnets creations

--- main.tf
+++ main.tf
@@ -39,6 +39,7 @@ module "vpc" {
   for_each = var.docdb
   subnets = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
   allow_db_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
+  engine_version = each.value["engine_version"]
   tags = local.tags
   env = var.env
   vpc_id = local.vpc_id
