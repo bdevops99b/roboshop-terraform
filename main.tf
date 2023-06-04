90502e9c39b90374e08ba344cd8f774cc456667f
bdevops99 <bdevops99@gmail.com>
2023-06-04 18:59:24 +0530

    Vpcs -subnets creations

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
-#module "vpc" {
-#  source = "git::https://github.com/bdevops99b/tf-module-vpc.git"
-#
-#  for_each = var.vpc
-#  cidr_block  = each.value["cidr_block"]
-#  subnets = each.value["subnets"]
-#  tags = local.tags
-#  env = var.env
-#  default_vpc_id = var.default_vpc_id
-#  default_vpc_cidr = var.default_vpc_cidr
-#  default_vpc_rtid = var.default_vpc_rtid
-#}
+module "vpc" {
+  source = "git::https://github.com/bdevops99b/tf-module-vpc.git"
+
+  for_each = var.vpc
+  cidr_block  = each.value["cidr_block"]
+  subnets = each.value["subnets"]
+  tags = local.tags
+  env = var.env
+  default_vpc_id = var.default_vpc_id
+  default_vpc_cidr = var.default_vpc_cidr
+  default_vpc_rtid = var.default_vpc_rtid
+}
 
 #module "app" {
 #  source = "git::https://github.com/bdevops99b/tf-module-app.git"
