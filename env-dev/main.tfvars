9b0fede11320c95c327cb36a75db6b790d0d5666
bdevops99 <bdevops99@gmail.com>
2023-06-04 22:07:03 +0530

    Vpcs -subnets creations

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -110,3 +110,13 @@ docdb = {
     instance_class = "db.t3.medium"
   }
 }
+
+rds = {
+  main = {
+    subnet_name    = "db"
+    allow_db_cidr  = "app"
+    engine_version = "5.7.mysql_aurora.2.11.2"
+    instance_count = 1
+    instance_class = "db.t3.small"
+  }
+}
--- main.tf
+++ main.tf
@@ -47,3 +47,19 @@ module "docdb" {
   vpc_id = local.vpc_id
   kms_arn = var.kms_arn
 }
+module "rds" {
+  source = "git::https://github.com/bdevops99b/tf-module-rds.git"
+
+  for_each       = var.rds
+  subnets        = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
+  allow_db_cidr  = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
+  engine_version = each.value["engine_version"]
+  instance_count = each.value["instance_count"]
+  instance_class = each.value["instance_class"]
+
+
+  tags    = local.tags
+  env     = var.env
+  vpc_id  = local.vpc_id
+  kms_arn = var.kms_arn
+}
