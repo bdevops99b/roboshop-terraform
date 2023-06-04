5edbae7fd55785ab35fb9f0ca7f4be09126abe1f
bdevops99 <bdevops99@gmail.com>
2023-06-04 19:51:01 +0530

    Vpcs -subnets creations

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -106,6 +106,8 @@ docdb = {
     subnet_name = "db"
     allow_db_cidr = "app"
     engine_version = "4.0.0"
+    instance_count = 1
+    instance_class = db.t3.medium
 
   }
 }
--- main.tf
+++ main.tf
@@ -40,6 +40,8 @@ module "docdb" {
   subnets = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
   allow_db_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
   engine_version = each.value["engine_version"]
+  instance_count = each.value["instance_count"]
+  instance_class = each.value["instance_class"]
   tags = local.tags
   env = var.env
   vpc_id = local.vpc_id
