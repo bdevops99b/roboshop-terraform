62cd92a1c5d37d32f929057f15cb6ac886176d7d
bdevops99 <bdevops99@gmail.com>
2023-07-04 13:53:40 +0530

    Docker - Kubernetes

--- main.tf
+++ main.tf
@@ -124,7 +124,7 @@ module "rabbitmq" {
 #}
 
 module "eks" {
-  source             = "git::https://github.com/bdevops99b//tf-module-eks.git"
+  source             = "git::https://github.com/bdevops99b/tf-module-eks.git"
   ENV                = var.env
   eks_version        = 1.27
   PRIVATE_SUBNET_IDS = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "app", null), "subnet_ids", null)
