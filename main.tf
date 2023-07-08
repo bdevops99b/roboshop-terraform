379d3ad56e8b26c18d91d468e2c8b54a4b91f9f4
bdevops99 <bdevops99@gmail.com>
2023-07-08 13:15:27 +0530

    Docker - Kubernetes

--- main.tf
+++ main.tf
@@ -145,7 +145,7 @@ data "aws_ami" "ami" {
 resource "aws_instance" "load" {
   ami = data.aws_ami.ami.id
   instance_type = "t3.medium"
-  vpc_security_group_ids = ["sg-05b81ed6eec97a66b"]
+  vpc_security_group_ids = ["sg-0266fc3fc1e7aea54"]
   tags = {
     Name = "load-runner"
   }
